c8be5de54435d990695dc23a2c69b2337653e1fb
lornaegele <lorenz.naegele@mail.de>
2025-08-21 12:28:34 +0200

    Add PricingTable

---
+++ app/subscription/page.tsx
@@ -0,0 +1,11 @@
+import { PricingTable } from "@clerk/nextjs";
+
+const Subscription = () => {
+  return (
+    <main className="min-2xl:w-2/3 items-center justify-center p-8">
+      <PricingTable />
+    </main>
+  );
+};
+
+export default Subscription;
