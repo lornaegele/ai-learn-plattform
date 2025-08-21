745af466258c4681207b80f5e1a5e6a9604223fb
lornaegele <lorenz.naegele@mail.de>
2025-08-21 09:41:03 +0200

    implement navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { navItems } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link
+          href={href}
+          key={label}
+          className={cn(pathname === href && "text-primary font-semibold")}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default NavItems;
